f1c94708d0c8c27d6fe1d86537af56ff4ef8ddb2
Prashu Verma <prashuverma012@gmail.com>
2025-06-06 00:26:04 +0530

    added ratelimiter

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import redisClient from './config/redisClient.js';
+import rateLimit from 'express-rate-limit';
+import RedisStore from 'rate-limit-redis';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -11,14 +13,37 @@ import Chapters from './src/routes/chapters.route.js';
 const app = express();
 const PORT = process.env.PORT || 5133;
 
-// middlewares
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api/v1/chapters', Chapters);
-
-app.listen(PORT, async() => {
-    console.log("listening..")
+async function startServer() {
     await Connection();
     await redisClient.connect();
-});
+
+    // req limiter
+    const limiter = rateLimit({
+        store: new RedisStore({
+            sendCommand: (...args) => redisClient.sendCommand(args),
+        }),
+        windowMs: 1 * 60 * 1000,
+        max: 30,
+        message: {
+            error: "Too many requests, try again after a moment"
+        },
+        standardHeaders: true,
+        legacyHeaders: false,
+    });
+
+    // middlewares
+    app.use(limiter);
+    app.use(cors());
+    app.use(express.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use('/api/v1/chapters', Chapters);
+
+    app.listen(PORT, async () => {
+        console.log(`listening on PORT: ${PORT}`);
+    });
+}
+
+startServer().catch(error => {
+    console.log("Server Error: ", error);
+})
--- backend/src/middlewares/customAdminCheck.js
+++ backend/src/middlewares/customAdminCheck.js
@@ -1,6 +1,4 @@
 export function adminCheck(req, res, next) {
-  
-  
   if ( req.headers["x-role"] !== "admin" ) {
     return res.status(403).json({ error: "Access denied." });
   }
